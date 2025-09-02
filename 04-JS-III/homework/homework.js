eadb69c49cda279e0e5f765fb74430827c3caa84
Mel <72985223+meli2000@users.noreply.github.com>
2025-09-01 22:57:28 -0500

    Update homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,20 +1,21 @@
-// No cambies los nombres de las funciones.
-
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0]
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length-1]
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length
 }
 
 
@@ -23,6 +24,13 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  
+  for(let i=0; i<array.length;i++){
+   
+    array[i]+=1
+ 
+  }
+ return array
 }
 
 
@@ -30,6 +38,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento)
+  return array
 }
 
 
@@ -38,6 +48,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento)
+  return array
 }
 
 
@@ -47,6 +59,8 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+   let unido=palabras.join(" ")
+   return unido
 }
 
 
@@ -54,13 +68,25 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-}
+  if (array.includes(elemento)){
+    return true
+  }else{
+    return false
+}}
 
 
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  // let suma=0
+  // for (const n of numeros) {
+  //   suma+=n
+  // }
+  // // return suma  ORR
+let inicial=0
+  let sumatoria=numeros.reduce((acumulador,valoractual)=>acumulador+valoractual,inicial,);
+  return sumatoria
 }
 
 
@@ -68,6 +94,14 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  let inicial=0
+// for (let i=0; i<resultadosTest.length;i++){
+//   resultadosTest[i]
+// }
+
+  let sumatoria=resultadosTest.reduce((acumulador,actual)=>acumulador+actual,inicial,);
+  let promedio =sumatoria/(resultadosTest.length)
+  return promedio
 }
 
 
@@ -75,6 +109,8 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  let maximo= Math.max(...numeros)
+  return maximo
 }
 
 
@@ -82,12 +118,28 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+ let argumentos= Array.from(arguments)
+  if(argumentos.length==0){
+    return 0
+  }else{
+
+  let multiplicar= argumentos.reduce((acumulador,valoractual)=>acumulador*valoractual,1,)
+  return multiplicar
+  }
+  
+  
+  
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  let resultado=arreglo.filter((items)=> items>18)
+  let final =resultado.length
+  return final
+  
+
 
 }
 
@@ -97,6 +149,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  if (numeroDeDia==1 || numeroDeDia==7) {
+    return "Es fin de semana"
+  }
+  else if(numeroDeDia>1 && numeroDeDia<7){
+return "Es dia Laboral"
+  }
   
 } 
 
